17ddd003a27fb571e136ca4f7722a587ca758c5c
Johannes Røsvik <j.rosvik@gmail.com>
2024-04-13 19:00:26 +0200

    rename project

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(get_ogd_data));
+    let app = Router::new().route("/", get(get_opengraph_tags));
     let addr = SocketAddr::from(([127, 0, 0, 1], 2339));
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on http://{}", addr);
@@ -20,27 +20,27 @@ async fn main() {
 }
 
 #[derive(Deserialize)]
-struct GetOgdDataParameters {
+struct GetOpengraphTagsParameters {
     url: String,
 }
 #[derive(Serialize)]
-struct OgdData {
+struct OpengraphTag {
     property: String,
     content: String,
 }
 
-async fn get_ogd_data(Query(query): Query<GetOgdDataParameters>) -> impl IntoResponse {
+async fn get_opengraph_tags(Query(query): Query<GetOpengraphTagsParameters>) -> impl IntoResponse {
     let url = query.url;
     let result = reqwest::get(url).await.expect("failed to fetch url");
     let data = result.text().await.expect("no data from url");
 
     let dom = tl::parse(&data, tl::ParserOptions::default()).expect("failed to parse html");
     let parser = dom.parser();
-    let elements = dom.query_selector("meta").expect("no meta tags not found");
+    let elements = dom.query_selector("meta").expect("no meta tags found");
 
-    let mut data: Vec<OgdData> = Vec::new();
+    let mut data: Vec<OpengraphTag> = Vec::new();
     elements.into_iter().for_each(|element| {
-        if let Some(ogd_data) = extract_ogd_data(element, parser) {
+        if let Some(ogd_data) = extract_opengraph_tag(element, parser) {
             data.push(ogd_data);
         }
     });
@@ -48,22 +48,22 @@ async fn get_ogd_data(Query(query): Query<GetOgdDataParameters>) -> impl IntoRes
     (StatusCode::OK, HeaderMap::new(), Json(data))
 }
 
-fn extract_ogd_data(element: NodeHandle, parser: &Parser) -> Option<OgdData> {
-    let element = element.get(parser).expect("element not found");
-    let tag = element.as_tag().expect("element is not a tag");
-    if let Some(Some(property)) = tag.attributes().get("property") {
+fn extract_opengraph_tag(node: NodeHandle, parser: &Parser) -> Option<OpengraphTag> {
+    let node = node.get(parser).expect("element not found");
+    let dom_tag = node.as_tag().expect("element is not a tag");
+    if let Some(Some(property)) = dom_tag.attributes().get("property") {
         let property = property.as_utf8_str().to_string();
         if !property.starts_with("og:") {
             return None;
         }
-        let content = tag
+        let content = dom_tag
             .attributes()
             .get("content")
             .expect("content attribute is unset")
             .expect("content is empty")
             .as_utf8_str()
             .into_owned();
-        return Some(OgdData { property, content });
+        return Some(OpengraphTag { property, content });
     }
     None
 }
